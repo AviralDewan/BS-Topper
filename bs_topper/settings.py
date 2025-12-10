a445adf3824c7c18f9c49aabe597bad3b1d15e79
Aviral Dewan <aviraldewan@gmail.com>
2025-12-10 12:31:40 +0530

    updated lot of things

--- bs_topper/settings.py
+++ bs_topper/settings.py
@@ -114,11 +114,17 @@ WSGI_APPLICATION = 'bs_topper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import environ
+import os
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db("DB_URL")
 }
 
 
